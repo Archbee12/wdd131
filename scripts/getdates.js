f3fd4810596974f88908a3aa35fcba389f905cea
Archbee12 <debbieokon32@gmail.com>
2024-09-22 02:28:30 +0100

    js

---
+++ scripts/getdates.js
@@ -0,0 +1,18 @@
+const currentYear = document.querySelector("#currentYear");
+
+// use the date object
+const today = new Date();
+
+
+currentYear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+let oLastModif = new Date(document.lastModified);
+lastModified.innerHTML = `Last Modified: <span class="highlight">${new Intl.DateTimeFormat(
+  "en-US",
+  {
+    dateStyle: "full"
+  }
+).format(today)}</span>`; 
+
+
+
