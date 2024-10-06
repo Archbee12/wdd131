21715a2c5001045add817b8055ef673d7c8670aa
Archbee12 <debbieokon32@gmail.com>
2024-10-06 02:35:54 +0100

    matched rubric requirements

--- scripts/place.js
+++ scripts/place.js
@@ -3,7 +3,7 @@ const windC = document.querySelector('#windchill');
 
 // Calling the Windchill function
 
-function windChill(temp, speed) {
+function calculateWindChill(temp, speed) {
 
   return (35.74 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) +
     0.3965 * temp * Math.pow(speed, 0.16));
@@ -13,7 +13,7 @@ const temperature = document.querySelector('#temp');
 const speed = document.querySelector('#wind');
 
 if (temperature <= 10 && speed > 4.8) {
-  windC.innerHTML = windChill(temperature, speed).toFixed(1) + "°C";
+  windC.innerHTML = calculateWindChill(temperature, speed).toFixed(1) + "°C";
 
 } else {
   windC.innerHTML = "N/A";
